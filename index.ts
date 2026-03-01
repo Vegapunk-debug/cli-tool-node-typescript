c39086994927129e15e257d1f00ea976c5aadc70
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-03-01 23:06:38 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const {Command} = require("commander")
+
+const program = new Command()
+
+program
+.command("greet <name>")
+.description("Greets the user")
+.action((name: string) => {
+    console.log(`Hello ${name}`)
+
+})
+
+program
+.command("add <num1> <num2>")
+.description("Adds two numbers")
+.action((num1: string, num2: string) => {
+    console.log("Sum:",parseFloat(num1) + parseFloat(num2))
+})
+
+program.parse()
